20213468d80d7e5ba44e74b46cba6a17b4c99967
Jan Pieter Waagmeester <jieter@jieter.nl>
2015-09-02 18:48:32 +0200

    Add updated bundles

--- site/bundle-csvplot.js
+++ site/bundle-csvplot.js
@@ -9576,49 +9576,7 @@
   this.d3 = d3;
 }();
 },{}],3:[function(require,module,exports){
-module.exports = function getRandomElement (arr) {
-	return arr[Math.floor(Math.random() * arr.length)];
-};
-
-},{}],4:[function(require,module,exports){
-var d3 = require('d3');
-
-var polarcsv = require('./polar-csv.js').export;
-var polartable = require('./polartable.js');
-
-var meta = d3.select('#meta').attr('class', 'meta');
-
-module.exports = function render_metadata (boat) {
-	d3.select('#name').html(boat.name || '<span class="text-muted">Geen naam bekend</span>');
-
-	meta.selectAll('.meta-item')
-		.data([
-			['zeilnummer', boat.sailnumber],
-			['type', boat.boat.type],
-			['lengte', boat.boat.sizes.loa + 'm'],
-			['diepgang', boat.boat.sizes.draft + 'm'],
-			['breedte', boat.boat.sizes.beam + 'm'],
-			'<br />',
-			['GPH', boat.rating.gph],
-			['offshore TN', boat.rating.triple_offshore.join(', ')],
-			['inshore TN', boat.rating.triple_inshore.join(', ')],
-			'<div class="table-container"></table>',
-			['polar (csv)', '<textarea>' + polarcsv(boat) + '</textarea>', 'polar']
-		]).enter().append('div').attr('class', 'meta-item');
-
-	meta.selectAll('.meta-item').html(function (d) {
-		if (typeof d === 'string') {
-			return d;
-		} else {
-			var className = 'meta-label' + (d.length === 3 ? ' ' + d[2] : '');
-			return '<span class="' + className + '">' + d[0] + '</span> ' +  d[1];
-		}
-	});
-	polartable(meta.select('.table-container'), boat);
-};
-
-},{"./polar-csv.js":5,"./polartable.js":7,"d3":2}],5:[function(require,module,exports){
-var CSV_PREAMBLE = 'twa/tws;';
+var CSV_PREAMBLE = 'twa/tws';
 var CSV_SEPARATOR = ';';
 
 function zeros (n) {
@@ -9632,13 +9590,13 @@ function float (n) {
 }
 function polarimport (str) {
 	str = str.trim();
-	console.log(str, str.indexOf(CSV_PREAMBLE));
+
 	if (str.indexOf(CSV_PREAMBLE) !== 0) {
 		throw 'CSV should start with ' + CSV_PREAMBLE;
 	}
 
 	// split by lines.
-	var rows = str.split(/\r?\n/).filter(function(s) { return s.length > 0; });
+	var rows = str.split(/\r?\n/).filter(function(s) { return s.length > 0 && s[0] != '#'; });
 
 	var polar = {
 		speeds: rows[0].split(CSV_SEPARATOR).slice(1).map(int),
@@ -9651,28 +9609,23 @@ function polarimport (str) {
 
 		polar.angles.push(twa);
 		polar[twa] = items.slice(1).map(float);
-
-
-	})
+	});
 
 	return polar;
 }
 
 
 function polarexport (data) {
-	data = data.vpp;
+	vpp = ('vpp' in data) ? data.vpp : data;
 
-	var ret = [CSV_PREAMBLE + data.speeds.join(';')];
-	ret.push(zeros(data.speeds.length + 1).join(';'));
-
-	console.log(ret);
+	var ret = [ [CSV_PREAMBLE].concat(data.speeds)];
+	ret.push(zeros(data.speeds.length + 1));
 
 	data.angles.forEach(function (angle) {
-		var row = [angle].concat(data[angle]);
-		ret.push(row.join(';'));
+		ret.push([angle].concat(data[angle]));
 	});
 
-	return ret.join('\n');
+	return ret.join('\n').map(function (row) { return row.join(CSV_SEPARATOR); });
 }
 
 module.exports = {
@@ -9680,7 +9633,7 @@ module.exports = {
 	import: polarimport
 }
 
-},{}],6:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
 var d3 = require('d3');
 require('d3-legend')(d3);
 
@@ -9792,12 +9745,14 @@ module.exports = function polarplot (container) {
 				var speed = vmg / Math.cos(angle);
 				return [angle, speed];
 			};
-
-			series.unshift(vmg2sog(vpp.beat_angle[i], vpp.beat_vmg[i]));
-
-			var run = vmg2sog(vpp.run_angle[i], -vpp.run_vmg[i]);
-			series.push(run);
-			run_data.push(run);
+			if (vpp.beat_angle) {
+				series.unshift(vmg2sog(vpp.beat_angle[i], vpp.beat_vmg[i]));
+			}
+			if (vpp.run_angle) {
+				var run = vmg2sog(vpp.run_angle[i], -vpp.run_vmg[i]);
+				series.push(run);
+				run_data.push(run);
+			}
 
 			return series.sort(function (a, b) { return a[0] - b[0]; });
 		});
@@ -9879,136 +9834,28 @@ module.exports = function polarplot (container) {
 	return plot;
 };
 
-},{"d3":2,"d3-legend":1}],7:[function(require,module,exports){
-module.exports = function polartable(container, boat) {
-	var vpp = boat.vpp;
-
-	// prepare data:
-	var header = ['Wind velocity'].concat(vpp.speeds.map(function (d) { return d + 'kts'; }));
-	var data = [
-		['Beat angles'].concat(vpp.beat_angle),
-		['Beat VMG'].concat(vpp.beat_vmg)
-	].concat(vpp.angles.map(function (angle) {
-		return [angle + '°'].concat(vpp['' + angle]);
-	})).concat([
-		['Run VMG'].concat(vpp.run_vmg),
-		['Gybe angles'].concat(vpp.run_angle)
-	]);
-
-	var table = container.selectAll('table').data([0]).enter()
-		.append('table')
-		.attr('class', 'table table-condensed polar-table');
-
-	var thead = table.selectAll('thead').data([0]).enter().append('thead');
-	var tbody = table.selectAll('tbody').data([0]).enter().append('tbody');
-
-	thead.selectAll('tr').data([0]).enter().append('tr')
-		.selectAll('th').data(header).enter().append('th')
-			.text(function (d) { return d; });
-
-	var rows = tbody.selectAll('tr').data(data)
-		.enter().append('tr')
-			.attr('class', function (d, i) {
-				if (i >= 2 && i <= 9) {
-					return 'twa-' + vpp.angles[i - 2];
-				} else {
-					return '';
-				}
-			});
-
-	var cells = rows.selectAll('td').data(function (d) { return d; })
-		.enter().append('td').attr('class', function (d, i) {
-			return (i > 0) ? 'tws-' + vpp.speeds[i - 1] : '';
-		});
-
-	cells.text(function (d) { return d; });
-};
-
-},{}],8:[function(require,module,exports){
+},{"d3":2,"d3-legend":1}],5:[function(require,module,exports){
 var d3 = require('d3');
 var polarplot = require('./src/polarplot.js');
-var render_metadata = require('./src/meta.js');
-var getRandomElement = require('./src/array-random.js');
+
+var import_polar = require('./src/polar-csv.js').import;
 
 var plot = polarplot('#chart');
 
-function match_boats (data, needle) {
-	needle = needle.toLowerCase();
 
-	for (var i in data) {
-		var value = data[i];
-		if (value.toLowerCase().indexOf(needle) !== -1) {
-			return true;
-		}
-	}
+function update() {
+    var csv = d3.select('textarea').property('value');
+
+    var polar = import_polar(csv);
+
+    console.log(polar);
+
+    plot.render(polar);
 }
 
-function display_boat (sailnumber) {
-	console.log('Loading ', sailnumber);
-	d3.json('data/' + sailnumber + '.json', function (boat) {
-		plot.render(boat);
-		render_metadata(boat);
+d3.select('textarea').on('keyup', update);
 
-		d3.selectAll('#list li').classed('active', function (d) {
-			return d[0] === boat.sailnumber;
-		});
-	});
-}
+update();
 
-var list = d3.select('#list');
-d3.json('index.json', function (response) {
-	list.selectAll('li')
-		.data(response)
-		.enter()
-			.append('li').attr('id', function (d) { return 'boat-' + d[0]; })
-			.append('a')
-			.attr({href: function (d) { return '#' + d[0]; }, class: 'boat'})
-			.on('click', function (d) {
-				console.log(d);
-				display_boat(d[0]);
-				d3.select('.row-offcanvas').classed('active', false);
-			})
-			.html(function (d) {
-				return '<span class="sailnumber">' + d[0] + '</span> ' + d[1] +
-				       '<br /><span class="type">' + d[2] + '</span>';
-			});
-
-	if (window.location.hash === '') {
-		// if window width is xs, do not randomly choose a boot but show
-		// selection list
-		if (window.innerWidth < 768) {
-			d3.select('.row-offcanvas').classed('active', true);
-			d3.select('#name').html('<i class="glyphicon glyphicon-arrow-left"></i> Kies een boot');
-		} else {
-			var sailnumber = getRandomElement(response)[0];
-			display_boat(sailnumber);
-		}
-	} else {
-		var sailnumber = window.location.hash.substring(1);
-		display_boat(sailnumber);
-	}
-});
-
-function search () {
-	var val = d3.select('input').property('value');
-
-	if (val === '') {
-		list.selectAll('a').attr('class', 'boat');
-	}
-	list.selectAll('a')
-		.attr('class', function (d) {
-			return 'boat' + (!match_boats(d, val) ? ' hidden' : '');
-		});
-	if (list.selectAll('a:not(.hidden)')[0].length === 1) {
-		plot.render(list.selectAll('a:not(.hidden)').data()[0]);
-	}
-}
-d3.select('input').on('keyup', search);
-d3.select('button').on('click', search);
-
-d3.select(window).on('resize', function () {
-	plot.resize();
-});
-
-},{"./src/array-random.js":3,"./src/meta.js":4,"./src/polarplot.js":6,"d3":2}]},{},[8])
+},{"./src/polar-csv.js":3,"./src/polarplot.js":4,"d3":2}]},{},[5])
 //# sourceMappingURL=bundle.js.map
