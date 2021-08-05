01670d5bb43d51312424f4f90cdfab59273068b6
Jan Pieter Waagmeester <jieter@zostera.nl>
2021-08-05 12:45:15 +0200

    Show stability index on meta items.

--- site/src/meta.js
+++ site/src/meta.js
@@ -5,14 +5,14 @@ var polartable = require('./polartable.js');
 
 var meta = d3.select('#meta').attr('class', 'meta');
 
-function metaItem (label, className, contents, title) {
+function metaItem(label, className, contents, title) {
     var className = 'meta-label' + (className ? ' ' + className : '');
     var title = title ? 'title="' + title + '"' : '';
 
-    return '<span class="' + className + '" ' + title  + '>' + label + '</span> ' + contents;
+    return '<span class="' + className + '" ' + title + '>' + label + '</span> ' + contents;
 }
 
-function table (data) {
+function table(data) {
     var header = '';
     var contents = '';
     for (var i = 0; i < data[0].length; i++) {
@@ -22,12 +22,15 @@ function table (data) {
     return '<table class="meta-table"><tr>' + header + '</tr><tr>' + contents + '</tr></table>';
 }
 
-module.exports = function render_metadata (boat, extended) {
+module.exports = function render_metadata(boat, extended) {
     d3.select('#name').html(boat.name || '<span class="text-muted">Name unknown</span>');
 
     var sizes = boat.boat.sizes;
 
-    var sailsTable = [['main', 'genoa'], [sizes.main + 'm²', sizes.genoa + 'm²']];
+    var sailsTable = [
+        ['main', 'genoa'],
+        [sizes.main + 'm²', sizes.genoa + 'm²']
+    ];
     if (sizes.spinnaker > 0) {
         sailsTable[0].push('spinnaker');
         sailsTable[1].push(sizes.spinnaker + 'm²');
@@ -40,27 +43,28 @@ module.exports = function render_metadata (boat, extended) {
     meta.selectAll('.meta-item')
         .data([
             table([
-                ['sail number', 'type', 'designer', 'owner'],
+                ['Sail number', 'Type', 'Designer', 'Owner'],
                 [boat.sailnumber, boat.boat.type, boat.boat.designer, boat.owner]
             ]),
             table([
-                ['length', 'beam', 'draft', 'displacement'],
+                ['Length', 'Beam', 'Draft', 'Displacement'],
                 [sizes.loa + 'm', sizes.beam + 'm', sizes.draft + 'm', sizes.displacement + ' kg']
-            ]),
-            ['Max sail area', table(sailsTable)],
+            ]), ['Max sail area', table(sailsTable)],
             '<br />',
-            table([['GPH', 'OSN'], [boat.rating.gph, boat.rating.osn]]),
-            ['offshore TN', boat.rating.triple_offshore.join(', '), 'Offshore triple number'],
-            ['inshore TN', boat.rating.triple_inshore.join(', '), 'Inshore triple number'],
+            table([
+                ['GPH', 'OSN', 'Stability index'],
+                [boat.rating.gph, boat.rating.osn, boat.boat.stability_index]
+            ]), ['Offshore TN', boat.rating.triple_offshore.join(', '), 'Offshore triple number'],
+            ['Inshore TN', boat.rating.triple_inshore.join(', '), 'Inshore triple number'],
             '<div class="table-container"></table>',
             metaItem(
-                'polar (csv)',
+                'Polar (csv)',
                 'polar',
                 '<textarea class="' + (extended ? 'csv-extended' : '') + '">' + polarcsv(boat, extended) + '</textarea>'
             )
         ]).enter().append('div').attr('class', 'meta-item');
 
-    meta.selectAll('.meta-item').html(function (d) {
+    meta.selectAll('.meta-item').html(function(d) {
         return (typeof d === 'string') ? d : metaItem(d[0], undefined, d[1], d[2]);
     });
 
