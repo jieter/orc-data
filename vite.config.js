09c71cf524e6e373db9736a9461bb9ffda435189
Jan Pieter Waagmeester <jieter@waarneming.nl>
2025-05-04 16:51:48 +0200

    Better practices?

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,14 @@
-import fs from 'fs';
-import path from 'path';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 // docs: https://vitejs.dev/guide/build.html
 export default defineConfig({
     base: '',
     publicDir: 'site',
-    server: {
-        fs: {
-            allow: ['site'], // avoid access denied when serving from site/
-        },
-    },
     build: {
-        outDir: './site/build/',
-        assetsDir: '.', // Put build files in outDir
+        outDir: './build/',
+        emptyOutDir: false,
         rollupOptions: {
-            input: {
-                index: 'site/src/index.js',
-            },
+            input: { index: 'src/index.js' },
             output: {
                 entryFileNames: `[name].js`,
                 chunkFileNames: `[name].js`,
@@ -40,19 +31,6 @@ export default defineConfig({
                     }
                 });
             },
-        },
-        {
-            name: 'filter-public-dir',
-            apply: 'build',
-            generateBundle() {
-                const skipPaths = ['src', 'data'];
-                skipPaths.forEach((p) => {
-                    const target = path.resolve(__dirname, 'site', p);
-                    if (fs.existsSync(target)) {
-                        fs.rmSync(target, { recursive: true, force: true });
-                    }
-                });
-            },
-        },
+        }
     ],
 });
